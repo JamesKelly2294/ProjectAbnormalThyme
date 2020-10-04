3bb1d8458530890db14ca357ac00e640c44f5f77
Jacob Ingalls <jacobingalls@yahoo.com>
2020-10-03 23:44:02 -0700

    Add money infomation to the gui

---
+++ Assets/GUI/MoneyInfoPanelSection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoneyInfoPanelSection : MonoBehaviour
+{
+
+    public TextMeshProUGUI balance;
+    public TextMeshProUGUI averageIncome;
+
+    MoneyManager moneyManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        balance.text = "" + string.Format("{0:#,0}", moneyManager.currentBalance);
+        averageIncome.text = "" + string.Format("{0:#,0.00}", moneyManager.averageIncomePerSecond) + "/s";
+    }
+}
