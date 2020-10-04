75880880008f286f726eb6067e9b79f53bb1edb2
Jacob Ingalls <jacobingalls@yahoo.com>
2020-10-03 20:45:14 -0700

    Add dynamic people

--- Assets/Rollercoaster/TrainCarPerson.cs
+++ Assets/Rollercoaster/TrainCarPerson.cs
@@ -11,20 +11,39 @@ public class TrainCarPerson : MonoBehaviour
 
     public int moneyInBank;
 
+    public SpenderType spenderType = SpenderType.green;
 
-    public int minPossibleDesriedMoneyInWallet, maxPossibleDesriedMoneyInWallet;
+    public SpriteRenderer body, face, hair;
 
-    public int minPossibleMoneyInBank, maxPossibleMoneyInBank;
+
+
+    public List<Sprite> greenBodySprites;
+    public List<Sprite> redBodySprites;
+    public List<Sprite> blueBodySprites;
+    public List<Sprite> purpleBodySprites;
+    public List<Sprite> hairSprites;
+    public List<Sprite> faceSprites;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        desriedMoneyInWallet = Random.Range(minPossibleDesriedMoneyInWallet, maxPossibleDesriedMoneyInWallet);
-        moneyInBank = Random.Range(minPossibleMoneyInBank, maxPossibleMoneyInBank);
-
-
         WithdrawFromBank();
+
+        List<Sprite> bodySprites = greenBodySprites;
+        switch (spenderType) {
+            case SpenderType.green:
+                bodySprites = greenBodySprites; break;
+            case SpenderType.red:
+                bodySprites = redBodySprites; break;
+            case SpenderType.blue:
+                bodySprites = blueBodySprites; break;
+            case SpenderType.purple:
+                bodySprites = purpleBodySprites; break;
+        }
+
+        face.sprite = faceSprites[Random.Range(0, faceSprites.Count)];
+        hair.sprite = hairSprites[Random.Range(0, hairSprites.Count)];
+        body.sprite = bodySprites[Random.Range(0, bodySprites.Count)];
     }
 
     // Update is called once per frame
@@ -40,4 +59,11 @@ public class TrainCarPerson : MonoBehaviour
         moneyInBank -= money;
         moneyPrinter.moneyToDispense += money;
     }
+}
+
+public enum SpenderType {
+    green = 0,
+    red,
+    blue,
+    purple
 }
