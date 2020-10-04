cc71c2730af75dda76b278866de9be478b31875c
Jacob Ingalls <jacobingalls@yahoo.com>
2020-10-04 12:17:19 -0700

    Add initial upgrades popup

---
+++ Assets/GUI/Research/ResearchPopUp.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResearchPopUp : MonoBehaviour
+{
+
+    public ResearchTab lab, hr, marketing;
+
+    public ResearchTabType currentTab = ResearchTabType.lab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleOpen() {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+    public void ChangeTab(ResearchTabType newTab) {
+        TabForResearchTabType(currentTab).gameObject.SetActive(false);
+        TabForResearchTabType(newTab).gameObject.SetActive(true);
+        currentTab = newTab;
+    }
+
+    public ResearchTab TabForResearchTabType(ResearchTabType tabType) {
+        switch (tabType) {
+            case ResearchTabType.lab:
+                return lab;
+            case ResearchTabType.hr:
+                return hr;
+            case ResearchTabType.marketing:
+                return marketing; 
+            default:
+                return lab;
+        }
+    }
+}
+
+public enum ResearchTabType {
+    lab, hr, marketing
+}
---
+++ Assets/GUI/Research/ResearchTab.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResearchTab : MonoBehaviour
+{
+    public ResearchPopUp popUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchTabToLab() {
+        popUp.ChangeTab(ResearchTabType.lab);
+    }
+
+    public void SwitchTabToHr() {
+        popUp.ChangeTab(ResearchTabType.hr);
+    }
+
+    public void SwitchTabToMarketing() {
+        popUp.ChangeTab(ResearchTabType.marketing);
+    }
+
+    public void Close() {
+        popUp.ToggleOpen();
+    }
+}
